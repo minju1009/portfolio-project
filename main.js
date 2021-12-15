8c34f531a53e8c90862f45356509bede509cb095
minju1009 <90900882+minju1009@users.noreply.github.com>
2021-12-15 23:35:58 +0900

    JS: home transparent

--- main.js
+++ main.js
@@ -4,12 +4,10 @@
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
-    console.log(`navbarHeight: ${navbarHeight}`);
     if(window.scrollY > navbarHeight){
         navbar.classList.add('navbar--dark');
     } else {
-        navbar.classList.remove('navbar--dark')
+        navbar.classList.remove('navbar--dark');
     }
 })
 
@@ -39,3 +37,16 @@ function scrollIntoView(selector){
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({behavior: 'smooth'});
 }
+
+// Make home slowly fade to transparent as the window scrolls down
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+
+document.addEventListener('scroll', () => {
+   if (scrollY > homeHeight/2) {
+       home.classList.add('home--transparent');
+   } else {
+       home.classList.remove('home--transparent');
+   }
+
+})
